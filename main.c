09ac9f2befa373f1b42039921a674e7da116288a
nicolas <nmpereira@ps.grupopanvel.com.br>
2025-11-09 20:03:29 -0300

    att main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 extern int yyparse(void);
 extern FILE *yyin;
@@ -7,28 +8,35 @@ extern FILE *yyout;
 
 int main(int argc, char **argv) {
     if (argc < 3) {
-        fprintf(stderr, "Uso: %s input.pokescript output.py\n", argv[0]);
+        fprintf(stderr, "Uso: %s input.pokescript output.js\n", argv[0]);
         return 1;
     }
 
+    /* entrada */
     yyin = fopen(argv[1], "r");
     if (!yyin) {
         perror("Erro ao abrir arquivo de entrada");
         return 1;
     }
 
-    yyout = fopen(argv[2], "w");
-    if (!yyout) {
-        perror("Erro ao abrir arquivo de saída");
-        fclose(yyin);
-        return 1;
+    /* saída: aceita "-" para stdout */
+    if (strcmp(argv[2], "-") == 0) {
+        yyout = stdout;
+    } else {
+        yyout = fopen(argv[2], "w");
+        if (!yyout) {
+            perror("Erro ao abrir arquivo de saída");
+            fclose(yyin);
+            return 1;
+        }
     }
 
-    if (yyparse() == 0) {
-        printf("Tradução concluída\n");
+    int rc = yyparse();
+    if (rc == 0) {
+        fprintf(stderr, "Tradução concluída\n");
     }
 
-    fclose(yyin);
-    fclose(yyout);
-    return 0;
+    if (yyin && yyin != stdin)  fclose(yyin);
+    if (yyout && yyout != stdout) fclose(yyout);
+    return rc; /* 0 ok, !=0 erro de sintaxe */
 }
